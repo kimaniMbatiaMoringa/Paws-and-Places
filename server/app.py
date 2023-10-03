19fcc0440a528fab81a9b4aec24883731cd020c2
ajackwere <austinewere59@gmail.com>
2023-10-03 10:46:50 +0300

    added delete and post on DogHouse and Reviews

--- server/app.py
+++ server/app.py
@@ -68,6 +68,33 @@ class DogHouseList(Resource):
         doghouses = DogHouse.query.all()
         return jsonify([doghouses.to_dict() for doghouse in doghouses])
 
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('location', type=str, required=True)
+        parser.add_argument('description', type=str, required=True)
+        parser.add_argument('price_per_night', type=str, required=True)
+        parser.add_argument('image_url', type=str, required=True)
+        parser.add_argument('amenities', type=str, required=True)
+
+        parser.add_argument('user_id', type=int, required=True)
+        args = parser.parse_args()
+
+        name = args['name']
+        location = args['location']
+        description = args['description']
+        price_per_night = args['price_per_night']
+        image_url = args['image_url']
+        amenities = args['amenities']
+        user_id = args['user_id']
+
+        doghouse = DogHouseList(
+            name=name, location=location, description=description, price_per_night=price_per_night, image_url=image_url, amenities=amenities, user_id=user_id
+        )
+        db.session.add(doghouse)
+        db.session.commit()
+        return jsonify(doghouse.user_id.to_dict()), 201
+
 
 api.add_resource(DogHouseList, '/doghouses')
 
@@ -101,6 +128,27 @@ class ReviewList(Resource):
 
 
 api.add_resource(ReviewList, '/reviews')
+
+
+class ReviewsById(Resource):
+    def get(self, id):
+        review = Review.query.get(id)
+        if review:
+            return jsonify(review.to_dict())
+        else:
+            return jsonify({"error": "Review not found"})
+
+    def delete(self, id):
+        review = Review.query.get(id)
+        if review:
+            db.session.delete(review)
+            db.session.commit()
+            return '', 204
+        else:
+            return jsonify({"review": "User not found"})
+
+
+api.add_resource(ReviewsById, '/reviews/<int:id>')
 
 
 if __name__ == '__main__':
